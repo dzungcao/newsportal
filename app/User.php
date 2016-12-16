94218a3c52e8c4a3c98e08095efcd54747a8f6cf
giaophong <dung.caon@vbsteam.com>
2016-12-16 15:44:43 +0700

    remove redundant stuffs

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','admin','manager','user','phone','mobile_phone','username','address','title','log'
+        'name', 'email', 'password','active'
     ];
 
     /**
@@ -26,56 +26,4 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function getAvatar(){
-        if(isset($this->avatar))
-            return $this->avatar;
-        return '/img/default-profile.jpg';
-    }
-
-    public function isAdmin(){
-        return $this->admin;
-    }
-    public function isManager(){
-        return !$this->admin && $this->manager;
-    }
-    public function isUser(){
-        return !$this->admin && !$this->manager && $this->user;
-    }
-    public function getRole(){
-        if($this->isAdmin()) return "Quản trị hệ thống";
-        if($this->isManager()) return "Nhân viên điều hành";
-        return null;
-    }
-    public function checkPermission($request){
-        $requestPath = $request->path();
-        $user = \Auth::user();
-        $path = storage_path() . "/app/permission.json";
-        $jsons = json_decode(file_get_contents($path), true); 
-        
-        foreach ($jsons as $json) {
-            # code...
-            foreach ($json['actions'] as $action) {
-                foreach ($action['urls'] as $url) {
-                    /*var_dump($url);
-                    var_dump($requestPath);*/
-                    if(fnmatch($url,$requestPath)){
-
-                        foreach ($action['roles'] as $role) {
-                            if($role == "admin" && $this->isAdmin()){
-                                return;
-                            }
-                            if($role == "manager" && $this->isManager()){
-                                return;
-                            }
-                            if($role == "user" && $this->isUser()){
-                                return;
-                            }
-                        }
-                    }
-                }
-            }
-        }        
-        abort(401,"You don't have permission to perform this action");
-
-    }
 }
